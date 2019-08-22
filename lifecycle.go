5143aca14428de3f1b2814d95bea530895973a14
Myles Horton <myles@getlantern.org>
2019-08-22 13:02:49 -0700

    added noop lifecycle

--- lifecycle.go
+++ lifecycle.go
@@ -21,3 +21,25 @@ type LifecycleListener interface {
 	OnStreamRead(int)
 	OnStreamClose()
 }
+
+// NoopLifecycleListener allows callers to use a noop listener.
+func NoopLifecycleListener() LifecycleListener {
+	return &noopLifecycleListener{}
+}
+
+type noopLifecycleListener struct{}
+
+func (n *noopLifecycleListener) OnSessionInit(context.Context) context.Context {
+	return context.Background()
+}
+func (n *noopLifecycleListener) OnTCPConnReceived()                  {}
+func (n *noopLifecycleListener) OnReadClientInitError(string)        {}
+func (n *noopLifecycleListener) OnDecodeClientInitError(string)      {}
+func (n *noopLifecycleListener) OnTCPStart(context.Context)          {}
+func (n *noopLifecycleListener) OnTCPEstablished(context.Context)    {}
+func (n *noopLifecycleListener) OnClientInitWritten(context.Context) {}
+func (n *noopLifecycleListener) OnClientInitRead(context.Context)    {}
+func (n *noopLifecycleListener) OnStreamInit(uint16)                 {}
+func (n *noopLifecycleListener) OnStreamWrite(int)                   {}
+func (n *noopLifecycleListener) OnStreamRead(int)                    {}
+func (n *noopLifecycleListener) OnStreamClose()                      {}
